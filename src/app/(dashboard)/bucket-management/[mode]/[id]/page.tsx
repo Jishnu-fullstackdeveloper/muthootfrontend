dc9a5522a4fcaed4e03b16b667648f2274763e57
Nesla-ms <nesla.m@gnxsolutions.in>
2025-01-14 14:41:16 +0530

    checking write access

---
+++ src/app/(dashboard)/bucket-management/[mode]/[id]/page.tsx
@@ -0,0 +1,8 @@
+import BucketManagementIndex from '@/views/pages/BucketManagment/BucketManagementIndex'
+import React from 'react'
+
+const page = () => {
+  return <BucketManagementIndex />
+}
+
+export default page

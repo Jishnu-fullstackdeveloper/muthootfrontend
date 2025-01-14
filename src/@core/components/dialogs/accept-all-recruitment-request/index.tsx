b0521befdc275e3d087517432d5fb4d2d1c64e32
siyad786 <siyad.m@gnxsolutions.in>
2025-01-14 12:28:13 +0530

    Working on recruitment overview confirm accept all requests

--- src/@core/components/dialogs/accept-all-recruitment-request/index.tsx
+++ src/@core/components/dialogs/accept-all-recruitment-request/index.tsx
@@ -6,78 +6,70 @@ import DialogTitle from '@mui/material/DialogTitle'
 import DialogActions from '@mui/material/DialogActions'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
-import { Tooltip, Box } from '@mui/material'
+import { Tooltip } from '@mui/material'
 
 // Type Imports
 import { toast } from 'react-toastify'
 
 // Component Imports
 import 'react-toastify/dist/ReactToastify.css'
+import custom_theme_settings from '@/utils/custom_theme_settings.json'
 
-type WarningDialogProps = {
+type delete_props = {
   open: boolean
   tooltipText: string
-  headingText: string
-  descriptionText: string
-  warningCount: number
-  confirmButtonText: string
-  cancelButtonText: string
-  onConfirm: () => void
-  onCancel: () => void
+  HeadingText: string
+  setOpen: (open: boolean) => void
+  setAcceptAllConfirmed: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const WarningDialog = ({
-  open,
-  tooltipText,
-  headingText,
-  descriptionText,
-  warningCount,
-  confirmButtonText,
-  cancelButtonText,
-  onConfirm,
-  onCancel
-}: WarningDialogProps) => {
+const ConfirmAcceptAllDialog = ({ open, setOpen, setAcceptAllConfirmed, tooltipText, HeadingText }: delete_props) => {
   return (
-    <Dialog
-      maxWidth='sm'
-      open={true}
-      onClose={onCancel}
-      sx={{
-        '& .MuiDialog-paper': { overflow: 'visible' },
-        '& .MuiTypography-root': { color: warningCount > 5 ? '#ff4c51' : '#ff9800' }
-      }}
-    >
-      <DialogTitle>
-        <Box display='flex' alignItems='center' gap={1}>
-          <Typography variant='h5'>{headingText}</Typography>
-          <Tooltip title={tooltipText}>
-            <i className='tabler-info-circle text-md'></i>
-          </Tooltip>
-        </Box>
-      </DialogTitle>
+    <>
+      <Dialog
+        // fullWidth
+        maxWidth='md'
+        scroll='body'
+        open={open}
+        onClose={() => setOpen(false)}
+        sx={{ '& .MuiDialog-paper': { overflow: 'visible' } }}
+      >
+        {/* <DialogCloseButton onClick={handleClose} disableRipple>
+            <i className='tabler-x' />
+          </DialogCloseButton> */}
 
-      <Typography variant='body1' sx={{ padding: '0 24px' }}>
-        {descriptionText}
-        <br />
-        <strong>Warnings: {warningCount}</strong>
-      </Typography>
+        <DialogTitle className='flex gap-1 flex-col text-left'>
+          <div className='flex gap-1 items-center'>
+            <Typography variant='h5'> {HeadingText ? HeadingText : 'Accept All Requests'}</Typography>
 
-      <DialogActions sx={{ p: 3 }}>
-        <Button variant='outlined' onClick={onConfirm} className='capitalize'>
-          {confirmButtonText}
-        </Button>
+            <Tooltip title={tooltipText} placement='right-start'>
+              <i className='tabler-info-circle text-md'></i>
+            </Tooltip>
+          </div>
 
-        <Button
-          variant='contained'
-          style={{ background: warningCount > 5 ? '#ff4c51' : '#ff9800' }}
-          onClick={onCancel}
-          className='capitalize'
-        >
-          {cancelButtonText}
-        </Button>
-      </DialogActions>
-    </Dialog>
+          <Typography variant='body1' className='flex flex-col text-left' pr={10}>
+            Are you sure you want accept all requests?
+            <br />
+          </Typography>
+        </DialogTitle>
+
+        <DialogActions className=' pbs-0 flex justify-end' sx={{ p: 3 }}>
+          <Button
+            variant='outlined'
+            // style={{ background: '#ff4c51' }}
+            onClick={() => setOpen(false)}
+            type='submit'
+            className='capitalize'
+          >
+            Cancel
+          </Button>
+          <Button variant='contained' onClick={() => setAcceptAllConfirmed(true)} className='capitalize'>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
-export default WarningDialog
+export default ConfirmAcceptAllDialog

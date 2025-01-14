c138ed5fee36f5880043cb72a8cdd0521ed2019a
Vishnu0726 <vishnu.s@gnxsolutions.in>
2025-01-14 13:57:05 +0530

    manual recruitment initial commit

--- src/views/pages/ResignationAndRecruitment/AddNewRequest.tsx
+++ src/views/pages/ResignationAndRecruitment/AddNewRequest.tsx
@@ -1,11 +1,10 @@
-
 import GeneratedForm from '@/form/generatedForms/addNewRequest'
 import React from 'react'
 
 type Props = {
-    mode: string
-    id: string
-  }
+  mode: string
+  id: string
+}
 
 const AddNewRequest: React.FC<Props> = ({ mode, id }) => {
   return (
@@ -16,4 +15,3 @@ const AddNewRequest: React.FC<Props> = ({ mode, id }) => {
 }
 
 export default AddNewRequest
-

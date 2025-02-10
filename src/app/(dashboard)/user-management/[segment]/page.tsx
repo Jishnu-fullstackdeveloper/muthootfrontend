2038bb04ce97fc5c40cf8264aca64e07d56ed534
Abhiram-R-24 <abhiramr.n@gnxsolutions.in>
2025-02-10 10:14:41 +0530

    Layout Update

--- src/app/(dashboard)/user-management/[segment]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function UserManagementPage({ params }: { params: { segment: string } }) {
-  return (
-    <div>
-      <h2>User Management: {params.segment}</h2>
-    </div>
-  )
-}

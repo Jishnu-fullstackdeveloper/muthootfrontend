b9e48a03d64704970a0e4b90fdf9e86106063980
Nesla-ms <nesla.m@gnxsolutions.in>
2025-02-10 09:58:22 +0530

    update user management

--- app/user-management/[...segments]/page.tsx
+++
@@ -1,5 +0,0 @@
-import UserManagementIndex from '@/views/pages/UserManagement/UserManagementIndex'
-
-export default function Page() {
-  return <UserManagementIndex />
-}
--- src/app/(dashboard)/user-management/[mode]/[id]/page.tsx
+++ src/app/(dashboard)/user-management/[mode]/[id]/page.tsx
@@ -1,6 +1,9 @@
 import UserManagementIndex from '@/views/pages/UserManagement/UserManagementIndex'
+import React from 'react'
 
 export default function Page() {
   return <UserManagementIndex />
 }
+
+
 

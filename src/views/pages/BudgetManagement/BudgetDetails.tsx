19a388c3aaf1498633dabf5fe49ce239dbb1f649
Abhiram-R-24 <abhiramr.n@gnxsolutions.in>
2025-03-27 12:35:02 +0530

    Budget Management
    1) #351_Budget Request Listing with approval status UI
    2) #350_Budget Increase Request Form UI

--- src/views/pages/BudgetManagement/BudgetDetails.tsx
+++ src/views/pages/BudgetManagement/BudgetDetails.tsx
@@ -1,27 +1,25 @@
-// 'use client'
+'use client'
 
-// import dynamic from 'next/dynamic'
-// import { usePathname } from 'next/navigation'
+import dynamic from 'next/dynamic'
+import { usePathname } from 'next/navigation'
 
-// // Dynamically import components to optimize loading
-// const NewBudgetRequest = dynamic(() => import('./NewBudgetRequest'), { ssr: false })
-// const ViewBudget = dynamic(() => import('./ViewBudget'), { ssr: false })
+// Dynamically import components to optimize loading
+const NewBudgetRequest = dynamic(() => import('./NewBudgetRequest'), { ssr: false })
+const ViewBudget = dynamic(() => import('./ViewBudget'), { ssr: false })
 
-// const BudgetDetails = () => {
-//   // Example: location.pathname = "/budget-management/add/new"
-//   const pathname = usePathname() // Gets the full pathname
-//   const segments = pathname.split('/') // Split by "/"
-//   const mode = segments[2] // Extract "add", "edit", or "view"
-//   const id = segments[3] // Extract "id"
+const BudgetDetails = () => {
+  // Example: location.pathname = "/budget-management/add/new"
+  const pathname = usePathname() // Gets the full pathname
+  const segments = pathname.split('/') // Split by "/"
+  const mode = segments[2] // Extract "add", "edit", or "view"
+  const id = segments[3] // Extract "id"
 
-//   return (
-//     <>
-//       {(mode === 'add' || mode === 'edit') && (
-//         <NewBudgetRequest mode={mode} id={id} />
-//       )}
-//       {mode === 'view' && <ViewBudget mode={mode} id={id} />}
-//     </>
-//   )
-// }
+  return (
+    <>
+      {mode === 'add' && <NewBudgetRequest mode={mode} id={id} />}
+      {mode === 'view' && <ViewBudget mode={mode} id={id} />}
+    </>
+  )
+}
 
-// export default BudgetDetails
+export default BudgetDetails

9d0071ba73333e97a366283e72121349d30d963d
Nesla-ms <nesla.m@gnxsolutions.in>
2025-05-12 17:41:53 +0530

    Job Posting UI - Settings and Customization Completed

---
+++ src/app/(dashboard)/hiring-management/job-posting-customization/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import JobPostingCustomization from '@/views/pages/HiringManagement/JobPostingCustomization/customization'
+
+const page = () => {
+  return <JobPostingCustomization />
+ 
+} 
+
+export default page
+
+ 

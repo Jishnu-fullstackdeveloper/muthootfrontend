27d71c4c1bf85c25560faef22d6bad458638ec02
Abhiram-R-24 <abhiramr.n@gnxsolutions.in>
2025-07-18 14:31:49 +0530

    Update BudgetDetails.tsx

--- src/views/pages/BudgetManagement/BudgetDetails.tsx
+++ src/views/pages/BudgetManagement/BudgetDetails.tsx
@@ -1,21 +1,24 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 import PositionMatrixTable from './PositionMatrixTable'
+import NewBudgetRequest from './NewBudgetRequest'
 
 const BudgetDetails = () => {
   // Example: location.pathname = "/budget-management/add/new"
   const pathname = usePathname() // Gets the full pathname
   const segments = pathname.split('/') // Split by "/"
   const mode = segments[4] // Extract "add", "edit", or "view"
-  // const searchParams = useSearchParams()
-  // const id = searchParams.get('id')
+  const searchParams = useSearchParams()
+  const id = searchParams.get('id')
+
   // const jobTitle = segments[5]
 
   return (
     <>
       {mode === 'view' && <PositionMatrixTable />}
+      {mode === 'add' && <NewBudgetRequest mode={mode} id={id} />}
       {/* {mode === 'view' && <PositionMatrixTable id={id} />} */}
       {/* {mode === 'department' && <ViewBudget mode={mode} id={id} jobTitle={jobTitle} />} */}
     </>

55a3ba7d3d40ea5c2601109c6624ad27250a45fb
Abhiram-R-24 <abhiramr.n@gnxsolutions.in>
2025-07-28 11:17:22 +0530

    Update budget.ts

--- src/redux/ApiUrls/budget.ts
+++ src/redux/ApiUrls/budget.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
   fetchBudgetDepartmentUrl: '/department-budget',
-  budgetIncreaseRequestUrl: '/budget-increase-request', // POST URL
+  budgetIncreaseRequestUrl: '/budget-increase-request', // POST URL.
   budgetIncreaseRequestListUrl: '/budget-increase-request', // GET URL for list
   budgetIncreaseRequestApproveRejectUrl: '/budget-increase-request/approve-reject?approvalRequestId=', // PUT URL
   budgetIncreaseRequestDetailUrl: '/budget-increase-request', // GET URL for detail by ID (with {id} parameter)
